d83abdf3f8483e31c0667bc15b82a5cd740819f5
Vitor Silva <vitordinissilva@gmail.com>
2021-06-24 20:34:41 +0100

    100% fun (mentira)

--- src/app/components/arena/arena.component.ts
+++ src/app/components/arena/arena.component.ts
@@ -13,7 +13,14 @@ export class ArenaComponent implements OnInit {
   router: Router;
   chartInf;
   chartInfI;
+  armasT;
+  armasI;
+  atk: number;
+  atkI: number;
   per = this.jogador.chart;
+  vez;
+  nvAtk;
+  probAcer;
 
   constructor(private pedir : DungeonService, private jogador : InfoPlayerService, router : Router) { this.router = router }
 
@@ -22,17 +29,18 @@ export class ArenaComponent implements OnInit {
       this.router.navigate(['']);
     }
 
+    this.vez = 0;
+
     this.pedir.myChart(this.jogador.IdPlayer).subscribe(
       arg => { this.chartInf = arg["data"].Personagens;
         this.chartInf = this.chartInf[this.per];
-        console.log(this.chartInf)
+        this.atk = this.chartInf.Atk;
       }
     );
 
     this.pedir.otherChar().subscribe(
       arg => { this.chartInfI = arg["data"];
-      console.log(this.chartInfI)
-      console.log(this.chartInfI)
+      this.atkI = this.chartInfI.Atk;
       }
     )
   }
@@ -42,16 +50,71 @@ export class ArenaComponent implements OnInit {
   }
 
   armas(){
-    console.log("ENTROU")
-    console.log(this.chartInf.ID)
-    console.log(this.chartInfI.ID)
+    if (this.armasT == undefined && this.armasI == undefined) {
+
     this.pedir.buscarArma(this.chartInf.ID).subscribe(
-      arg => console.log(arg["data"].Armas[0])
-    )
+      arg => {this.armasT = arg["data"].Armas;
+      for (let i = 0; i < this.armasT.length; i++) {
+        this.atk = this.atk + +this.armasT[i].Atk;
+      }
+    })
 
     this.pedir.buscarArma(this.chartInfI.ID).subscribe(
-      arg => console.log(arg)
-    )
+      arg => {this.armasI = arg["data"].Armas
+      for (let i = 0; i < this.armasI.length; i++) {
+        this.atkI = this.atkI + +this.armasI[i].Atk;
+      }
+    })
+    }
+  }
+
+  batalha() {
+
+    if (this.chartInf.Vida >= 0 && this.chartInfI.Vida >= 0) {
+      if (this.vez % 2 == 0) {
+        // vez do jogador
+        this.vez++;
+
+        this.probAcer = (this.chartInf.Int + (this.chartInf.Int * 3)) * 2;
+        this.nvAtk = Math.floor(Math.random() * 100);
+        console.log(this.nvAtk)
+        if ( (this.nvAtk >= 0 && this.nvAtk <= this.chartInf.Int) ) {
+          alert(this.chartInf.Nome + ', Ataque Critico');
+          this.chartInfI.Vida = this.chartInfI.Vida - this.atk;
+        }
+        else if ( (this.nvAtk > this.chartInf.Int && this.nvAtk <= this.probAcer) ) {
+          alert(this.chartInf.Nome + ', Ataque Normal');
+          this.chartInfI.Vida = this.chartInfI.Vida - Math.round(this.atk / 2);
+        }
+        else {
+          alert(this.chartInf.Nome + ', Falha');
+        }
+      }
+      else {
+        // vez inimigo
+        this.vez++;
+
+        this.probAcer = (this.chartInfI.Int + (this.chartInfI.Int * 3)) * 2;
+        this.nvAtk = Math.floor(Math.random() * 100);
+        console.log(this.nvAtk)
+        if ( (this.nvAtk >= 0 && this.nvAtk <= this.chartInfI.Int) ) {
+          alert(this.chartInfI.Nome + ', Ataque Critico');
+          this.chartInf.Vida = this.chartInf.Vida - this.atkI;
+        }
+        else if ( (this.nvAtk > this.chartInfI.Int && this.nvAtk <= this.probAcer) ) {
+          alert(this.chartInfI.Nome + ', Ataque Normal');
+          this.chartInf.Vida = this.chartInf.Vida - Math.round(this.atkI / 2);
+        }
+        else {
+          alert(this.chartInfI.Nome + ', Falha');
+        }
+      }
+    }
+    else {
+      alert ("Acabou!");
+      this.ngOnInit();
+    }
+
   }
 
 }
